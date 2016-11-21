9edec202286d29556c67707e6baee754200ae3fe
root <root@x.massivesoft.net>
2016-11-21 07:11:19 +0100

    committing working versions

--- install-squid-with-ssl-support.sh
+++ install-squid-with-ssl-support.sh
@@ -11,7 +11,7 @@ wget https://raw.githubusercontent.com/sebastian-king/squid3-ssl/master/squid-ss
 apt-get source squid
 apt-get build-dep squid
 apt-get install devscripts build-essential fakeroot libssl-dev #dpkg-dev #for debian
-patch squid-ssl.patch  # as of squid 3.5 --with-open-ssl has become --with-openssl
+patch < squid-ssl.patch  # as of squid 3.5 --with-open-ssl has become --with-openssl
 
 cd "${squid_version}"
 ./configure
@@ -19,3 +19,5 @@ debuild -us -uc -b
 cd ../
 apt-get install squid-langpack
 dpkg -i squid_"${squid_version}"_"${architecture}".deb squid-common_"${squid_version}"_all.deb
+
+#viola, https_port is now enabled and working in squid
