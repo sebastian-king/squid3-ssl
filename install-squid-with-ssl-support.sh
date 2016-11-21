bd620348eff722ccecf7356f2afbeb76a2504c5c
root <root@x.massivesoft.net>
2016-11-21 07:02:34 +0100

    added installation script and patch file

---
+++ install-squid-with-ssl-support.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+squid_version=$(cat apt-cache policy squid | grep Candidate: | awk '{print $2}');
+
+architecture=$(dpkg --print-architecture);
+
+cd /usr/src/
+
+apt-get source squid
+apt-get build-dep squid
+apt-get install devscripts build-essential fakeroot libssl-dev #dpkg-dev #for debian
+patch # as of squid 3.5 --with-open-ssl has become --with-openssl
+
+cd "${squid_version}"
+./configure
+debuild -us -uc -b
+cd ../
+apt-get install squid-langpack
+dpkg -i squid_"${squid_version}"_"${architecture}".deb squid-common_"${squid_version}"_all.deb
