6d6167e9edc063eaddadd7815f5c9f79c361b969
Sebastian King <sebastian-king@users.noreply.github.com>
2017-10-03 13:30:02 -0500

    Update install-squid-with-ssl-support.sh

--- install-squid-with-ssl-support.sh
+++ install-squid-with-ssl-support.sh
@@ -22,4 +22,4 @@ cd ../
 apt-get install squid-langpack
 dpkg -i squid_"${squid_version}"_"${architecture}".deb squid-common_"${squid_version}"_all.deb
 
-#viola, https_port is now enabled and working in squid
+#et viola, https_port is now enabled and working in squid
